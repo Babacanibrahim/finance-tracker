25fb5e40746a9c902acb634337e6a7ca93b8556a
Babacanİbrahim <babacan-1907@outlook.com.tr>
2025-06-27 14:34:18 +0300

    Sifresini unutanlar icin register formuna gizli soru ozelligi getirdim. O sekilde verify yapabilecegiz. Veritabanina hem soruyu hem de cevabi kaydettim.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
 from flask_mysqldb import MySQL
-from wtforms import Form,StringField,TextAreaField,PasswordField,validators,ValidationError
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators,ValidationError,SelectField
 from passlib.hash import sha256_crypt
 from functools import wraps
 from flask_wtf import FlaskForm
@@ -40,6 +40,13 @@ def length_check_password(form, field):
         raise ValidationError("En az 3 karakter olmalı.")
     elif len(field.data) > 20 :
         raise ValidationError("En fazla 5 karakter olmalı.")
+    
+#Şifre yenileme formu
+class ForgotPassword(FlaskForm):
+    #Form
+    username=StringField("Kullanıcı adınız : ",validators=[validators.DataRequired()])
+    secret_quest=SelectField("Gizli sorunuzu seçiniz :",choices=[("pet","Evcil hayvanınızın adı ?"),("child","Çocuğunuzun adı ?"),("music","En sevdiğiniz müzik türü ?"),("secretkey","Kendinize ait bir gizli anahtar ?")])
+
 
 #Login Form
 class LoginForm(FlaskForm):
@@ -53,7 +60,9 @@ class RegisterForm(FlaskForm):
   
     #Form
     username=StringField("* Kullanıcı adınız :",validators=[length_check_username,validators.DataRequired(message="Kullanıcı adı boş olamaz.")])
-    email=StringField(" E posta :",validators=[validators.Email(message="Geçerli bir mail adresi giriniz.")])
+    email=StringField(" E posta :",validators=[validators.optional(),validators.Email(message="Geçerli bir mail adresi giriniz.")])
+    secret_quest=SelectField("Gizli sorunuzu seçiniz (şifrenizi unutmanız durumunda kullanılır opsiyoneldir)",choices=[("","Soru seçiniz."),("pet","Evcil hayvanınızın adı ?"),("child","Çocuğunuzun adı ?"),("music","En sevdiğiniz müzik türü ?"),("secretkey","Kendinize ait bir gizli anahtar ? (önerilmez)")],validate_choice=False)
+    answer=StringField("Gizli sorunun cevabı :")
     password=PasswordField("* Parola :",validators=[validators.DataRequired(message="Parola kısmı boş olamaz."),length_check_password,validators.EqualTo("confirm",message="Lütfen parolalarınızı kontrol edin")])
     confirm=PasswordField("* Parolanız tekrar :",validators=[validators.DataRequired()])
 
@@ -80,6 +89,11 @@ def expense():
 @login_required
 def dashboard():
     return render_template("dashboard.html")
+
+#Şifremi unuttum
+@app.route("/forgotpassword",methods=["GET","POST"])
+def forgot_password():
+    pass
 
 #Login 
 @app.route ("/login",methods=("GET","POST"))
@@ -117,21 +131,23 @@ def register():
     form=RegisterForm(request.form)
     if request.method=="POST" and form.validate():
         cursor=mysql.connection.cursor()
-        query="INSERT INTO users (username , email , password) VALUES (%s,%s,%s)"
+        query="INSERT INTO users (username , answer , question , email , password) VALUES (%s,%s,%s,%s,%s)"
         query_exist="SELECT * FROM users WHERE username=%s"
 
         username=form.username.data
         email=form.email.data
         password=sha256_crypt.hash(form.password.data)
+        answer=form.answer.data
+        question=form.secret_quest.data
 
         result=cursor.execute(query_exist,(username,))
 
         if result>0:
             flash("Böyle bir isme sahip kullanıcı mevcut. Şifrenizi unuttuysanız şifremi unuttum'la devam edebilirsiniz.","danger")
-            return render_template("register.html")
+            return redirect(url_for("register"))
     
         else:
-            cursor.execute(query,(username,email,password))
+            cursor.execute(query,(username,answer,question,email,password))
             mysql.connection.commit()
             cursor.close()
             flash("Kaydınız başarıyla oluşturuldu.","success")
