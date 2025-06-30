b540f3f6c1eaab5bdd377ba41b65a8c848fcb98d
Babacanİbrahim <babacan-1907@outlook.com.tr>
2025-06-30 12:33:09 +0300

    Veritabanini mysqlden sqllite ile yapmaya karar verdim. Sifremi unuttum fonksiyonlari ilerletilir. Veritabani sifirdan tekrar yapilir.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
 from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators,ValidationError,SelectField
 from passlib.hash import sha256_crypt
 from functools import wraps
@@ -9,13 +10,47 @@ app = Flask(__name__)
 
 app.secret_key="babacanfinans"
 
+
+#Mysql konfigürasyonları
+"""
 app.config["MYSQL_HOST"]= "localhost"
 app.config["MYSQL_USER"]= "root"
 app.config["MYSQL_PASSWORD"]= ""
 app.config["MYSQL_DB"]= "finance_tracker"
 app.config["MYSQL_CURSORCLASS"]= "DictCursor"
 
-mysql = MySQL(app)
+mysql = MySQL(app)"""
+
+
+# Yeni SQLite ayarı
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///finance_tracker.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# User Tablosu DB için
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(100))
+    question = db.Column(db.String(100))
+    answer = db.Column(db.String(100))
+
+# Expense Tablosu DB için
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category=db.Column(db.String(50))
+    amount=db.Column(db.Numeric(10,2),nullable=False)
+    date=db.Column(db.DateTime , nullable=False)
+
+# Income Tablosu DB için
+class Income(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category=db.Column(db.String(50))
+    amount=db.Column(db.Numeric(10,2),nullable=False)
+    date=db.Column(db.DateTime , nullable=False)
+
 
 #Login Decorator
 def login_required(f):
@@ -37,17 +72,23 @@ def length_check_username(form, field):
         
 def length_check_password(form, field):
     if len(field.data) < 5:
-        raise ValidationError("En az 3 karakter olmalı.")
+        raise ValidationError("En az 5 karakter olmalı.")
     elif len(field.data) > 20 :
         raise ValidationError("En fazla 5 karakter olmalı.")
     
-#Şifre yenileme formu
+#Şifre yenileme kullanıcı doğrulama formu
 class ForgotPassword(FlaskForm):
+    
     #Form
     username=StringField("Kullanıcı adınız : ",validators=[validators.DataRequired()])
     secret_quest=SelectField("Gizli sorunuzu seçiniz :",choices=[("pet","Evcil hayvanınızın adı ?"),("child","Çocuğunuzun adı ?"),("music","En sevdiğiniz müzik türü ?"),("secretkey","Kendinize ait bir gizli anahtar ?")])
     answer=StringField("Gizli sorunun cevabı :")
 
+#Şifre yenileme
+class ChangePassword(FlaskForm):
+
+    password=PasswordField("* Parola :",validators=[validators.DataRequired(message="Lütfen bir parola giriniz."),length_check_password,validators.EqualTo("confirm",message="Lütfen parolalarınızı kontrol edin")])
+    confirm=PasswordField("* Parolanız tekrar :",validators=[validators.DataRequired(message="Lütfen parolanızı kontrol ediniz.")])
 
 #Login Form
 class LoginForm(FlaskForm):
@@ -86,6 +127,10 @@ def income():
 def expense():
     return render_template ("expense.html")
 
+@app.route("/changepassword")
+def changepassword():
+    return render_template("changepassword.html")
+
 @app.route("/dashboard")
 @login_required
 def dashboard():
@@ -94,7 +139,21 @@ def dashboard():
 #Şifremi unuttum
 @app.route("/forgotpassword",methods=["GET","POST"])
 def forgot_password():
-    form=ForgotPassword()
+    form=ForgotPassword(request.form)
+    if request.method=="POST" and form.validate():
+        username=form.username.data
+        question=form.secret_quest.data
+        answer=form.answer.data
+        user=User.query.filter_by(username=username , answer=answer , question=question).first()
+
+        if user :
+            form_chng=ChangePassword()
+            return redirect(url_for("changepassword"),form_chng=form_chng)
+
+
+        else:
+            flash("Böyle bir kullanıcı maalesef bulunamadı.")
+
     return render_template("forgotpassword.html",form=form)
 
 #Login 
@@ -104,59 +163,45 @@ def login ():
     if request.method=="POST" and form.validate():
         username=form.username.data
         password=form.password.data
-        cursor=mysql.connection.cursor()
-        query="SELECT * FROM users WHERE username=%s"
-        result=cursor.execute(query,(username,))
 
-        if result>0:
-            user=cursor.fetchone()
-            if not sha256_crypt.verify(password, user["password"]):
+        user = User.query.filter_by(username=username).first()
+        if user and sha256_crypt.verify(password, user.password):
+            flash("Giriş başarılı","success")
+            session["logged_in"]=True
+            session["username"]=username
+            return redirect(url_for("index"))
+        
+        elif user and not sha256_crypt.verify(password, user.password):
                 flash("Şifreniz yanlış","danger")
-                cursor.close()
                 return render_template("login.html",form=form)
-            else:
-                flash("Giriş başarılı","success")
-                session["logged_in"]=True
-                session["username"]=username
-                cursor.close()
-                return redirect(url_for("index"))
-                
-
         else:
             flash("Böyle bir kullanıcı adı bulunamadı.","danger")
-            cursor.close()
+
     return render_template("login.html",form=form)
 
 #Register
 @app.route("/register",methods=["GET","POST"])
 def register():
-    form=RegisterForm(request.form)
-    if request.method=="POST" and form.validate():
-        cursor=mysql.connection.cursor()
-        query="INSERT INTO users (username , answer , question , email , password) VALUES (%s,%s,%s,%s,%s)"
-        query_exist="SELECT * FROM users WHERE username=%s"
+     form = RegisterForm(request.form)
+     if request.method == "POST" and form.validate():
+        username = form.username.data
+        password = sha256_crypt.hash(form.password.data)
+        email = form.email.data
+        question = form.secret_quest.data
+        answer = form.answer.data
 
-        username=form.username.data
-        email=form.email.data
-        password=sha256_crypt.hash(form.password.data)
-        answer=form.answer.data
-        question=form.secret_quest.data
+        existing_user = User.query.filter_by(username=username).first()
 
-        result=cursor.execute(query_exist,(username,))
-
-        if result>0:
-            flash("Böyle bir isme sahip kullanıcı mevcut. Şifrenizi unuttuysanız şifremi unuttum'la devam edebilirsiniz.","danger")
-            return redirect(url_for("register"))
-    
+        if existing_user:
+            flash("Bu kullanıcı adı daha önceden alınmış.", "danger")
         else:
-            cursor.execute(query,(username,answer,question,email,password))
-            mysql.connection.commit()
-            cursor.close()
-            flash("Kaydınız başarıyla oluşturuldu.","success")
+            new_user = User(username=username, password=password, email=email, question=question, answer=answer)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Kayıt başarılı! Giriş yapabilirsiniz.", "success")
             return redirect(url_for("login"))
-    else:
-        return render_template("register.html",form=form)
-
+     else:
+        return render_template("register.html", form=form)
 #Logout
 @app.route("/logout")
 def logout():
@@ -165,4 +210,6 @@ def logout():
 
 #Flask
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
